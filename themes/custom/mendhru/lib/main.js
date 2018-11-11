cdcfb4303800dafe06e572a0e2cffb39a8a82f65
kishor <kishork14@gmail.com>
2018-11-11 09:49:40 +0530

    modified user profile

--- themes/custom/mendhru/lib/main.js
+++ themes/custom/mendhru/lib/main.js
@@ -9,6 +9,34 @@ jQuery(document).ready(function($) {
       $('.closebtn').click(function(){
         $('#sideNavigation').css('width' , '0');
         $("#main").css('marginLeft',"0");
-      });  
+      });
+      
+      $('.like-button').on('click',function(){
+          var getUrlParameter = function getUrlParameter(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+        
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+        };
+        $.ajax({
+          type:'post',
+          url:'profile-activity ',
+          data:{
+            memberid:getUrlParameter('memberId'),
+            type:'profilelike'
+          },
+          success:function(responce){
+            console.log(responce);
+          }
+        });
 
+
+      });
 });
