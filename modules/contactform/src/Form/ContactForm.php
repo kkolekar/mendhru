575bf24bd8f39452ec8a6cc23c1c6d47191b243d
kish0808 <kishork14@gmail.com>
2018-11-19 12:46:25 +0530

    modified frontend of all form

--- modules/contactform/src/Form/ContactForm.php
+++ modules/contactform/src/Form/ContactForm.php
@@ -1,8 +1,8 @@
 <?php
-  /**
-   * @file
-   * Contains \Drupal\contactform\form\contactform.
-  */
+    /**
+     * @file
+     * Contains \Drupal\contactform\form\contactform.
+    */
   namespace Drupal\contactform\Form;
   use Drupal\Core\Form\FormBase;
   use Drupal\Core\Form\FormStateInterface;
@@ -14,64 +14,62 @@
      * {@inheritdoc}
     */
     public function getFormId() {
-    // dpm('check');
+     // dpm('check');
       return 'contact_Form';
     }
     /**
      * {@inheritdoc}
     */
     public function buildForm(array $form, FormStateInterface $form_state) {
-     
-    $form['name'] = array(     
-      '#type' => 'textfield',
-      '#title' => $this->t('Full Name'),
-    );
-
-    $form['contact'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('mobile number'),
-    );
-
-    $form['message'] = array(
-      '#type' => 'textarea',
-      '#title' => $this->t('message'),
-    );
-
-    $form['Submit'] = array(
-      '#type' => 'submit',
-      '#input'=> 'TRUE',
-      '#value' => $this->t('send'),
-    );
-
-    return $form;
-  }
-  /**
-   * {@inheritdoc}
-  */
-
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-  }
-  
-  /**
-    * {@inheritdoc}
-  */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-    $id = $user->id();    
-    $name = $form_state->getValue('name');
-    $contact = $form_state->getValue('contact');
-    $message= $form_state->getValue('message');
-    
-
-
-    db_insert('contact')
-     ->fields(array(
-      'id' => $user->id,
-      'name' =>  $name,
-      'contact' =>  $contact, 
-      'message' =>  $message   
-    ))->execute();
-    drupal_set_message(" Mesaage send successfully"); 
       
-  }
-} 
+      $form['name'] = array(     
+        '#type' => 'textfield',
+        '#placeholder' => t('Name'),
+      );
+
+      $form['contact'] = array(
+        '#type' => 'textfield',
+        '#placeholder' => t('Mobile Number'),
+      );
+
+      $form['message'] = array(
+        '#type' => 'textarea',
+        '#placeholder' => t('Message'),
+      );
+
+      $form['Submit'] = array(
+        '#type' => 'submit',
+        '#input'=> 'TRUE',
+        '#value' => $this->t('send'),
+      );
+
+      return $form;
+    }
+    /**
+     * {@inheritdoc}
+    */
+
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+    }
+  
+    /**
+      * {@inheritdoc}
+    */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+      $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+      $id = $user->id();    
+      $name = $form_state->getValue('name');
+      $contact = $form_state->getValue('contact');
+      $message= $form_state->getValue('message');
+      
+      db_insert('contact')
+      ->fields(array(
+        'id' => $user->id,
+        'name' =>  $name,
+        'contact' =>  $contact, 
+        'message' =>  $message   
+      ))->execute();
+      drupal_set_message(" Mesaage send successfully"); 
+        
+    }
+  } 
